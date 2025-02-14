9be36f61a822b666b80b9e4b86e297140fa0b313
gtnoble <garretnoble@gmail.com>
2025-02-14 10:30:28 -0600

    added normality and randomness tests, untested code

--- src/stats.ts
+++ src/stats.ts
@@ -1,4 +1,5 @@
 import statistics from '@stdlib/stats';
+import kstest from '@stdlib/stats/kstest';
 
 export interface ControlLimitsType {
   mean: number,
@@ -32,4 +33,73 @@ export function countsChartSetupParams (
   const lowerControlLimit = poissonDistribution.quantile(mean, boundaryQuantile);
 
   return {mean: mean, upperControlLimit: upperControlLimit, lowerControlLimit: lowerControlLimit};
+}
+
+export function normalityTest (
+  values: number[],
+  alpha: number = 0.001
+) {
+  const sampleMean = statistics.base.mean(values.length, values, 1);
+  const sampleStandardDeviation = statistics.base.stdev(values.length, 1, values, 1);
+  return kstest(
+    values, 
+    statistics.base.dists.normal.cdf, 
+    sampleMean, 
+    sampleStandardDeviation, 
+    {alpha: alpha}
+  );
+}
+
+export function sampleMedian (
+  values: number[]
+) {
+  return statistics.base.mediansorted(
+    values.length, 
+    [...values].sort((a, b) => a - b), 
+    1
+  );
+}
+
+export function runsRandomnessTest (
+  values: number[],
+  boundaryQuantile: 0.001
+) {
+  const median = sampleMedian(values);
+  
+  const signs = values
+    .map((value) => value - median)
+    .filter((difference) => difference !== 0)
+    .map((difference) => difference > 0);
+    
+  let numPositiveRuns = 0;
+  let numNegativeRuns = 0;
+  let runSign: boolean | undefined = undefined;
+  
+  for (const sign of signs) {
+    if (! (sign === runSign)) {
+      if (sign) {
+        numPositiveRuns++;
+      }
+      else {
+        numNegativeRuns++;
+      }
+      runSign = sign;
+    }
+  }
+  
+  const totalRuns = numPositiveRuns + numNegativeRuns;
+  
+  const mean = (2 * numPositiveRuns * numNegativeRuns / signs.length) + 1;
+  const variance = ((mean - 1) * (mean - 2)) / (signs.length - 1);
+  const standardDeviation = Math.sqrt(variance);
+  
+  const normalDistribution = statistics.base.dists.normal;
+  const minumumExpectedRuns = normalDistribution.quantile(boundaryQuantile, mean, standardDeviation);
+  const maximumExpectedRuns = normalDistribution.quantile(1 - boundaryQuantile, mean, standardDeviation);
+  
+  return {
+    totalRuns: totalRuns,
+    lowerRunsLimit: minumumExpectedRuns,
+    upperRunsLimit: maximumExpectedRuns
+  };
 }
